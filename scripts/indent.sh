1e0d1332862477277ae8e499296b7155d18a5b81
mmrbulbul <mmrbulbul@gmail.com>
2024-09-09 19:25:54 +0900

    update: directory name

--- scripts/indent.sh
+++ scripts/indent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-python3 -m isort $(find rag-systems -name "*.py")
-python3 -m autopep8 --in-place $(find rag-systems -name "*.py")
+python3 -m isort $(find rag_systems -name "*.py")
+python3 -m autopep8 --in-place $(find rag_systems -name "*.py")
