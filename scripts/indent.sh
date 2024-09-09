fdb2d56c1753836f6c701391e5f056c336105415
mmrbulbul <mmrbulbul@gmail.com>
2024-09-09 17:02:17 +0900

    update: relative path

--- scripts/indent.sh
+++ scripts/indent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-python3 -m isort **/**.py
-python3 -m autopep8 --in-place **/**.py
+python3 -m isort $(find rag-systems -name "*.py")
+python3 -m autopep8 --in-place $(find rag-systems -name "*.py")
