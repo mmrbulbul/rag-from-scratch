db1dab57d06f9d5ed14af4e4bd97d70009d02612
mmrbulbul <mmrbulbul@gmail.com>
2024-09-12 19:34:55 +0900

    update: file renamed

---
+++ rag_systems/utils/initializer.py
@@ -0,0 +1,9 @@
+
+
+class Initializer:
+    def __init__(self, config):
+        self.llm_agent = None
+        self.qa_validator = None
+        self.qa_generator = None
+        self.document_reader = None
+        self.embedding_model = None
